52e17567787bc307e715e278779794f4cf898a95
AdyRock <38184396+AdyRock@users.noreply.github.com>
2022-07-25 16:00:37 +0100

    Added Lock

---
+++ drivers/lock_hub/device.js
@@ -0,0 +1,113 @@
+/* jslint node: true */
+
+'use strict';
+
+const HubDevice = require('../hub_device');
+
+class LockHubDevice extends HubDevice
+{
+
+    /**
+     * onInit is called when the device is initialized.
+     */
+    async onInit()
+    {
+        await super.onInit();
+
+        this.registerCapabilityListener('locked', this.onCapabilityLocked.bind(this));
+
+        const dd = this.getData();
+        this.homey.app.registerHomeyWebhook(dd.id);
+
+        this.log('LockHubDevice has been initialized');
+    }
+
+    /**
+     * onAdded is called when the user adds the device, called just after pairing.
+     */
+    async onAdded()
+    {
+        this.log('LockHubDevice has been added');
+    }
+
+    /**
+     * onRenamed is called when the user updates the device's name.
+     * This method can be used this to synchronise the name to the device.
+     * @param {string} name The new name
+     */
+    async onRenamed(name)
+    {
+        this.log('LockHubDevice was renamed');
+    }
+
+    async onSettings({ oldSettings, newSettings, changedKeys })
+    {
+        // Called when settings changed
+    }
+
+    // this method is called when the Homey device has requested a position change ( 0 to 1)
+    async onCapabilityLocked(value, opts)
+    {
+        if (value)
+        {
+            return this._operateBot('lock');
+        }
+        else
+        {
+            return this._operateBot('unlock');
+        }
+
+        return true;
+    }
+
+    async _operateBot(command)
+    {
+        const data = {
+            command,
+            parameter: 'default',
+            commandType: 'command',
+        };
+
+        return super.setDeviceData(data);
+    }
+
+    async getHubDeviceValues()
+    {
+        try
+        {
+            const data = await this._getHubDeviceValues();
+            if (data)
+            {
+                this.setAvailable();
+                this.homey.app.updateLog(`Lock Hub got: ${this.homey.app.varToString(data)}`, 2);
+
+                this.setCapabilityValue('locked', data.lockState === 'locked ').catch(this.error);
+            }
+        }
+        catch (err)
+        {
+            this.homey.app.updateLog(`getHubDeviceValues: : ${this.homey.app.varToString(err)}`);
+            this.setUnavailable(err.message);
+        }
+    }
+
+    async processWebhookMessage(message)
+    {
+        try
+        {
+            const dd = this.getData();
+            if (dd.id === message.context.deviceMac)
+            {
+                // message is for this device
+                this.setCapabilityValue('lockState', message.context.detectionState === 'LOCKED').catch(this.error);
+            }
+        }
+        catch (err)
+        {
+            this.homey.app.updateLog(`processWebhookMessage error ${err.message}`);
+        }
+    }
+
+}
+
+module.exports = LockHubDevice;
