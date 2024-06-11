6597cc83cdc47efcb56ca1e29fbf2fd878162e41
Adrian Rockall <38184396+AdyRock@users.noreply.github.com>
2024-06-11 18:10:29 +0100

    feat: Trigger On/Off Flow card in Bot Press Mode, Added support for the Robot Vacuum Cleaner S10

--- drivers/robot_vaccum_hub/driver.js
+++ drivers/robot_vaccum_hub/driver.js
@@ -23,7 +23,7 @@ class HubVacuumDriver extends HubDriver
 
 	async onPairListDevices({ oAuth2Client })
 	{
-		return this.getHUBDevices(oAuth2Client, ['WoSweeper', 'WoSweeperMini']);
+		return this.getHUBDevices(oAuth2Client, ['WoSweeper', 'WoSweeperMini', 'Robot Vacuum Cleaner S10']);
 	}
 
 	async triggerStateChanged(device, tokens, state)
