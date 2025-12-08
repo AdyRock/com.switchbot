261c59bd91a50265523caeab0561db91274d88bd
Adrian Rockall <ady@rockalls.uk>
2025-12-08 00:46:35 +0000

    feat(climate_panel_hub): add Climate Panel Hub device with capabilities for temperature, humidity, motion detection, and battery measurement
    - Implemented ClimatePanelHubDevice class extending HubDevice with methods for initialization, adding, renaming, and processing webhook messages.
    - Added functionality to retrieve and update device values including temperature, humidity, motion detection, and battery status.
    - Created driver.compose.json to define the device's name, capabilities, and energy requirements.
    - Developed driver.js to handle OAuth2 initialization and device pairing.
    - Included image asset for the device.

---
+++ drivers/climate_panel_hub/device.js
@@ -0,0 +1,125 @@
+/* jslint node: true */
+
+'use strict';
+
+const HubDevice = require('../hub_device');
+
+class ClimatePanelHubDevice extends HubDevice
+{
+
+	/**
+	 * onInit is called when the device is initialized.
+	 */
+	async onInit()
+	{
+		await super.onInit();
+
+		const dd = this.getData();
+		this.homey.app.registerHomeyWebhook(dd.id).catch(this.error);
+
+		this.log('ClimatePanelHubDevice has been initialized');
+	}
+
+	/**
+	 * onAdded is called when the user adds the device, called just after pairing.
+	 */
+	async onAdded()
+	{
+		this.log('ClimatePanelHubDevice has been added');
+	}
+
+	/**
+	 * onRenamed is called when the user updates the device's name.
+	 * This method can be used this to synchronise the name to the device.
+	 * @param {string} name The new name
+	 */
+	async onRenamed(name)
+	{
+		this.log('ClimatePanelHubDevice was renamed');
+	}
+
+	async getHubDeviceValues()
+	{
+		try
+		{
+			const data = await this._getHubDeviceValues();
+			if (data)
+			{
+				this.setAvailable();
+				this.homey.app.updateLog(`ClimatePanelHubDevice Hub got: ${this.homey.app.varToString(data)}`, 3);
+
+				this.setCapabilityValue('measure_temperature', data.temperature).catch(this.error);
+				this.setCapabilityValue('measure_humidity', data.humidity).catch(this.error);
+				this.setCapabilityValue('alarm_motion', data.moveDetected).catch(this.error);
+
+				if (data.battery)
+				{
+					this.setCapabilityValue('measure_battery', data.battery).catch(this.error);
+				}
+			}
+			this.unsetWarning().catch(this.error);;
+		}
+		catch (err)
+		{
+			this.homey.app.updateLog(`ClimatePanelHubDevice getHubDeviceValues: ${this.homey.app.varToString(err.message)}`, 0);
+			this.setWarning(err.message);
+		}
+	}
+
+	async processWebhookMessage(message)
+	{
+		try
+		{
+			const dd = this.getData();
+			if (dd.id === message.context.deviceMac)
+			{
+				// message is for this device
+				if (message.context.temperature)
+				{
+					let { temperature } = message.context;
+					if (message.context.scale && message.context.scale !== 'CELSIUS')
+					{
+						// Convert F to C
+						temperature = ((temperature - 32) * 5) / 9;
+					}
+
+					this.setCapabilityValue('measure_temperature', temperature).catch(this.error);
+				}
+
+				if (message.context.humidity)
+				{
+					this.setCapabilityValue('measure_humidity', message.context.humidity).catch(this.error);
+				}
+
+				if (message.context.detectionState)
+				{
+					this.setCapabilityValue('alarm_motion', message.context.detectionState == 'DETECTED').catch(this.error);
+				}
+
+				if (message.context.battery)
+				{
+					if (!this.hasCapability('measure_battery'))
+					{
+						try
+						{
+							await this.addCapability('measure_battery');
+						}
+						catch(err)
+						{
+							this.log(err);
+						}
+					}
+
+					this.setCapabilityValue('measure_battery', message.context.battery).catch(this.error);
+				}
+			}
+		}
+		catch (err)
+		{
+			this.homey.app.updateLog(`ClimatePanelHubDevice processWebhookMessage error ${err.message}`, 0);
+		}
+	}
+
+}
+
+module.exports = ClimatePanelHubDevice;
